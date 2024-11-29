a480620ce12fb6d287eff1ba53384f74f8402bff
”Erkin <erkenoof@gmail.com>
2024-11-29 14:38:17 +0500

    Изменен db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=feedback_db;charset=utf8;unix_socket=/tmp/mysql.sock', 'root', 'SaintBelkin');
+    $db = new PDO('mysql:host=localhost;dbname=feedback_db;charset=utf8;unix_socket=/tmp/mysql.sock', 'ur_db_name', 'ur_password');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Соединение установлено!";
 } catch (PDOException $e) {
